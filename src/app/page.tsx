29f4d7ee0af6b9b4947086294a32cd81197c8551
deepanjalitripathi <deepanjalitripathi7@gmail.com>
2024-05-24 16:41:05 +0530

    nav bar built

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        
-      Hello Deepanjali
-       
-      </div>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">Hello Deepanjali</div>
     </main>
   )
 }
